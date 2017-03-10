54db4dd4d366b2f1f2888bd1d8f2402012207a6b
PXNati <nati@perimeterx.com>
2017-03-10 13:48:58 +0200

    Fixed some minor bugs.

--- src/app/theme/services/baUtil.js
+++ src/app/theme/services/baUtil.js
@@ -118,43 +118,45 @@
          * Responsible for executing an AJAX request.
          * If the returned data is in JSON format, the function automatically decodes it.
          *
-         * @param url The URL destination.
-         * @param callback A callback called when the server responds.
-         * @param type The request type (GET, POST). Defaults to 'GET'.
-         * @param data Additional HTTP parameters. Defaults to an empty string.
-         * @return mixed In case of a sucessful request (200 OK),
+         * @param url {string} The URL destination.
+         * @param callback {function} A callback called when the server responds.
+         * @param type {string} The request type (GET, POST). Defaults to 'GET'.
+         * @param query {string} Additional HTTP parameters. Defaults to an empty string.
+         * @return {mixed} In case of a sucessful request (200 OK),
          * 				 the function will call the parameter callback
          * 				 using the parsed response data.
          * 				 In case the data has been JSON encoded, the function will decode it.
          */
-        this.ajax = function (url, callback, type, data) {
+        this.ajax = function (url, callback, type, query) {
             // Trim everything!
             url = url.trim();
             type = type.trim();
 
             // Make sure the data always has a 'submit' value appended to it
-            if(data.indexOf('submit=') == -1) {
+            if(query.indexOf('submit=') == -1) {
                 // It doesn't, append the parameter
-                data += '&submit=1';
+                query += '&submit=1';
             }
-            if(data.indexOf('ajax=') == -1) {
+            // Make sure the JSON parameter is in the data
+            if(query.indexOf('json=') == -1) {
                 // It doesn't, append the parameter
-                data += '&ajax=1';
+                query += '&json=1';
             }
-            if(data.indexOf('token=') == -1) {
+            // Make sure we are sending the CSRF token as well
+            if(query.indexOf('token=') == -1) {
                 // It doesn't, append the parameter
-                data += '&token=' + csrf_token;
+                query += '&token=' + csrf_token;
             }
 
             // Send the AJAX request
             $.ajax({
                 type: type,
                 url: url,
-                data: data,
+                data: query,
             }).done( function(data, textStatus, request) { // Success
                 // Check if we should redirect
                 if( data['redirect'] ) {
-                    redirect(data['redirect']);
+                    this.redirect(data['redirect']);
                 }
 
                 // Store the new CSRF token
--- src/app/theme/theme.constants.js
+++ src/app/theme/theme.constants.js
@@ -7,6 +7,7 @@
 var web_root = window.location['protocol'] + '//' + window.location['host'] + '/'; // The web root of the system
 var system_root = web_root; // The links web root (web_root + index.php)
 var frontend_root = web_root + FRONTEND_PATH; // The front end web root
+csrf_token = null;
 
 (function () {
     'use strict';
